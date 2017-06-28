a60867b81e78c1a975dda41baa9b4e46eaa22426
elalyavi <qwerty9515@mail.ru>
2017-06-29 02:08:52 +0300

    Update excursion.rb

--- app/models/excursion.rb
+++ app/models/excursion.rb
@@ -1,10 +1,7 @@
 class Excursion < ActiveRecord::Base
   belongs_to :city
-
-    accepts_nested_attributes_for :city, allow_destroy: true
-
-   
-
-   validates :name, :describe, presence: true 
-   validates :price, presence: true
+  
+  accepts_nested_attributes_for :city, allow_destroy: true
+  validates :name, :describe, presence: true 
+  validates :price, presence: true
 end
